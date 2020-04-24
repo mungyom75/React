c059f83a10c45aabcc6068c53884fe38321ec874
mungyom <mungyom@naver.com>
2020-04-24 12:46:23 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
+const customers=[{
+  'id':'2020192',
+  'name':'mgkim',
+  'image':'https://placeimg.com/64/64/any',
+  'birthday':'2020192',
+  'gender':'male',
+  'job':'programmer'
+},
+{
+  'id':'2020193',
+  'name':'mgkim',
+  'image':'https://placeimg.com/64/64/1',
+  'birthday':'2020192',
+  'gender':'male',
+  'job':'programmer'
+},
+{
+  'id':'2020194',
+  'name':'mgkim',
+  'image':'https://placeimg.com/64/64/2',
+  'birthday':'2020192',
+  'gender':'male',
+  'job':'programmer'
+}]
 class App extends Component{
   render(){
     return(
-      <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>adfasfasf</h2>
+      <div>
+        {
+          customers.map(c=> {
+            return (
+              <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              />
+            );
+          })
+        }
+      
       </div>
     );
   }
